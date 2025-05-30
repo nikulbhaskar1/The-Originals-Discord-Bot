86e560084db3a83aa5054ad3efc15b0d0957f78b
nikulbhaskar1 <nikulbhaskar9414@gmail.com>
2025-05-31 02:50:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,3 +45,20 @@ COLORS = {
     'music': 0x9b59b6,
     'moderation': 0xe74c3c
 }
+
+# YouTube DL options for music
+YTDL_OPTS = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+    'extractaudio': True,
+    'audioformat': 'mp3'
+}
